41b719a8b2e0dca5ffe30f99a528046b1bf59d7a
Guillaume Raffy <guillaume.raffy@univ-rennes1.fr>
2022-11-30 13:32:03 +0100

    made improtools package installable using `pip install -e ./improtools`

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='improtools',
+      version=1.00,
+      description='image processing tools',
+      url='https://github.com/g-raffy/improtools',
+      author='Guillaume Raffy',
+      author_email='guillaume.raffy.work@gmail.com',
+      license='MIT',
+      packages=['improtools'],
+      zip_safe=False)
